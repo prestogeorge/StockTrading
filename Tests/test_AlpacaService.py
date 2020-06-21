b331d3603b95575d52d74ae73f89e9f0d4345661
prestogeorge <60333379+prestogeorge@users.noreply.github.com>
2020-06-21 18:14:17 -0400

    todo: figure out how to mock requests.get

--- Tests/test_AlpacaService.py
+++ Tests/test_AlpacaService.py
@@ -1,10 +1,14 @@
 import unittest
 from unittest.mock import MagicMock
-
+from unittest.mock import patch
 from AlpacaClient.AlpacaClientConfig import AlpacaClientConfig
+from AlpacaService import AlpacaService
 
 
 class test_AlpacaService(unittest.TestCase):
-    def test_getAccount_returnsAccount(self):
+    @patch('requests.get')
+    def test_getAccount_returnsAccount(self, mocked_requests):
         alpacaClientConfig = AlpacaClientConfig('YOUR-KEY', 'YOUR-SECRET-KEY', 'YOUR-BASE-URL')
+        #TODO: figure out how to mock requests.get
+        #account = AlpacaService.AlpacaService.getAccount(alpacaClientConfig)
         self.assertEqual(True, True)
