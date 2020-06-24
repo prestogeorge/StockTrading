c86d594b45b6b28eaa57bb3669a633a1512cc81f
prestogeorge <60333379+prestogeorge@users.noreply.github.com>
2020-06-23 20:23:30 -0400

    Position attributes

--- Response/Alpaca/Position.py
+++ Response/Alpaca/Position.py
@@ -1,2 +1,24 @@
+from dataclasses import dataclass
+
+from undictify import type_checked_constructor
+
+
+@type_checked_constructor(convert=True, skip=True)
+@dataclass
 class Position(object):
-    pass
+    asset_id: str
+    symbol: str
+    exchange: str
+    asset_class: str
+    avg_entry_price: float
+    qty: int
+    side: str
+    market_value: float
+    cost_basis: float
+    unrealized_pl: float
+    unrealized_plpc: float
+    unrealized_intraday_pl: float
+    unrealized_intraday_plpc: float
+    current_price: float
+    lastday_price: float
+    change_today: float
